96c4460ff618564ca4bb16eab66548ac1209cb7d
rizkyputrapb <rizkyputrapb@gmail.com>
2020-11-11 17:08:23 +0700

    add video bg for landing page

---
+++ app/src/main/java/com/example/zahfitclient/LandingFragment.java
@@ -0,0 +1,81 @@
+package com.example.zahfitclient;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import com.example.zahfitclient.databinding.FragmentLandingBinding;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LandingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LandingFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    FragmentLandingBinding binding;
+
+    public LandingFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LandingFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LandingFragment newInstance(String param1, String param2) {
+        LandingFragment fragment = new LandingFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_landing, container, false);
+        Uri uri = Uri.parse("android.resource://"+ getActivity().getPackageName() +"/"+R.raw.intro_bg);
+        binding.videoView.setVideoURI(uri);
+        binding.videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
+        binding.videoView.start();
+        return binding.getRoot();
+    }
+}
--- app/src/main/java/com/example/zahfitclient/MainActivity.java
+++ app/src/main/java/com/example/zahfitclient/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.zahfitclient;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.zahfitclient.databinding.ActivityMainBinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
     }
 }
