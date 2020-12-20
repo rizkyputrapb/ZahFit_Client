729637f764e10e3047f0e9de18ed52cbd7931aa1
pazcalll <yazeedarifin@gmail.com>
2020-12-20 15:42:21 +0700

    LogoutTest

--- app/src/androidTest/java/com/example/zahfitclient/LogoutTest.java
+++ app/src/androidTest/java/com/example/zahfitclient/LogoutTest.java
@@ -28,34 +28,29 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
 
-public class VogellaEspressoTest {
-
+public class LogoutTest {
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityTestRule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
-
+//    Test Passed
     @Test
-    public void VogellaEspressoTest() throws InterruptedException {
+    public void LogoutTest() throws InterruptedException {
         onView(withText(startsWith("Login"))).perform(click());
+        onView(withId(R.id.txt_Email)).perform(click());
+        Thread.sleep(500);
         onView(withId(R.id.txt_Email)).perform(typeText("yz@gmail.com"));
+        Thread.sleep(500);
+        onView(withId(R.id.txt_Password)).perform(click());
+        Thread.sleep(500);
         onView(withId(R.id.txt_Password)).perform(typeText("101010"));
         closeSoftKeyboard();
         onView(withText(startsWith("Login"))).perform(click());
         Thread.sleep(3000);
         onView(withContentDescription("Open navigation drawer")).perform(click());
-        Thread.sleep(1000);
-        onView(withId(R.id.txt_usernameNAV)).check(matches(withText(containsString("yz"))));
-        onView(withId(R.id.txt_useremailNAV)).check(matches(withText(containsString("yz@gmail.com"))));
+        Thread.sleep(500);
+        onView(withText(startsWith("Settings"))).perform(click());
+        Thread.sleep(500);
+        onView(withText(startsWith("Log Out"))).perform(click());
+        onView(withId(R.id.btnLogin)).check(matches(withText(containsString("Login"))));
+        onView(withId(R.id.btnRegister)).check(matches(withText(containsString("Register"))));
     }
-
-//    @Test
-//    public void bademail() throws InterruptedException {
-//        onView(withText(startsWith("Login"))).perform(click());
-//        onView(withId(R.id.txt_Email)).perform(typeText("rizkyputra"));
-//        onView(withId(R.id.txt_Password)).perform(typeText("12345678"));
-//        closeSoftKeyboard();
-//        onView(withText(startsWith("Login"))).perform(click());
-//        Thread.sleep(1000);
-//        onView(withText("Login Failure: The email address is badly formatted")).inRoot(new ToastMatcher())
-//                .check(matches(isDisplayed()));
-//    }
 }
---
+++ app/src/androidTest/java/com/example/zahfitclient/SettingsTest.java
@@ -0,0 +1,4 @@
+package com.example.zahfitclient;
+
+public class SettingsTest {
+}
